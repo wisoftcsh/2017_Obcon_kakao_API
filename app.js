04761d8e7262e943a1c3ab1652d2a503bfca9449
Sunho Choi <csh8724@gmail.com>
2017-09-19 00:59:45 +0900

    kakao API server

--- app.js
+++ app.js
@@ -18,20 +18,20 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -42,12 +42,19 @@ app.use(function(err, req, res, next) {
 });
 
 const options = {
-    key: fs.readFileSync('./private.key'),
-    cert: fs.readFileSync('./publickey.cer')
+    key: fs.readFileSync('./privkey1.pem', 'utf8'),
+    cert: fs.readFileSync('./cert1.pem', 'utf8')
 };
 
 https.createServer(options, app).listen(11011);
-// app.listen(11011);
-push.webpush();
+// app.listen(11012);
+setInterval(() => {
+    let check = push.check();
+    check.then((result) => {
+        if (result) {
+            push.webpush();
+        }
+    });
+}, 10000);
 
 module.exports = app;
--- service/push.js
+++ service/push.js
@@ -3,20 +3,64 @@
  */
 const MySQL = require('promise-mysql');
 const MySQLConfig = require('../config/mysql');
+const request = require('request');
+
 let connection;
-exports.webpush = () => {
+
+exports.webpush = async () => {
+  const result = await search();
+  for (let i of result) {
+    push(i);
+  }
+};
+
+const search = () => {
+  return new Promise(resolve => {
+    let token = [];
+    MySQL.createConnection(MySQLConfig)
+      .then((conn) => {
+        connection = conn;
+        return connection.query('select * from token');
+      }).then((result) => {
+      for (let i of result) {
+        token.push(i.token);
+      }
+      connection.end();
+      return resolve(token);
+    });
+  });
+};
+
+exports.check = async () => {
+  const checkEmergency = await emergency();
+  return parseInt(checkEmergency) > 140;
 
 };
 
-const push = () => {
+const emergency = () => {
+  return new Promise(resolve => {
     MySQL.createConnection(MySQLConfig)
-        .then(function (conn) {
-            connection = conn;
-            return connection.query('select * from token');
-        }).then((result) => {
-        console.log(result);
-        // for (let i of result) {
-        //
-        // }
-    });
-}
+      .then((conn) => {
+        connection = conn;
+        return connection.query('select * from sensing where sensorId = 7 ORDER BY inputtime desc limit 1')
+      })
+      .then((result) => {
+        for (let i of result) {
+          resolve(i.sensingValue);
+        }
+      });
+  });
+};
+
+const push = (token) => {
+  request.post({
+    headers: {
+      'Authorization': 'key=insert server key',
+      'Content-Type': 'application/json'
+    },
+    url: 'https://fcm.googleapis.com/fcm/send',
+    body: '{"to":"' + token + '", "priority":"high","data":{"status": "최선호님의 상태가 매우 위급합니다. 119에 신고해주세요!!!!!"}}'
+  }, function (error, response, body) {
+    console.log(body);
+  });
+};
