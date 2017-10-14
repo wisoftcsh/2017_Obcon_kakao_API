494721c56d8a586a490aaed9a9e2d5cb369aa7a5
wisoftcsh <csh8724@gmail.com>
2017-10-14 21:51:44 +0900

    kakao API server

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,7 @@ router.put('/add', (req, res) => {
                 console.log(err);
                 console.log(result);
                 connection.end();
+                res.end();
             })
         });
 });
@@ -26,11 +27,16 @@ router.get('/value', (req, res) => {
     MySQL.createConnection(MySQLConfig)
         .then(function (conn) {
             connection = conn;
-            return connection.query('select sensorName, sensingValue, S.inputTime from sensing S, ' +
-                '(select sensorId, max(inputTime) inputTime from sensing group by sensorId) P, sensor SS' +
-                ' where S.sensorId = P.sensorId and S.inputTime = P.inputTime and SS.sensorId = S.sensorId;');
+            return connection.query('select sensorName, round(avg(sensingValue)) sensingValue from sensing S, ' +
+                '(select sensorId, max(inputTime) inputTime from sensing group by sensorId and inputTime + 0 > current_timestamp() - 100) P, sensor SS' +
+                ' where S.sensorId = P.sensorId and S.inputTime = P.inputTime and SS.sensorId = S.sensorId and S.inputTime + 0 > current_timestamp() - 100 group by sensorName;');
         }).then((result) => {
         let string = '[사용자 : 최선호]\n님의 현재 상태입니다.\n';
+        if(result.length === 0) {
+            string += '\n현재 사용자는 디바이스를 ' +
+                '착용하고 있지 않습니다.\n' +
+                '또는 디바이스 전원을 확인해주세요..';
+        }
         for (let i of result) {
             string = string + i.sensorName + ' : ' + i.sensingValue + '\n';
         }
--- service/push.js
+++ service/push.js
@@ -8,59 +8,104 @@ const request = require('request');
 let connection;
 
 exports.webpush = async () => {
-  const result = await search();
-  for (let i of result) {
-    push(i);
-  }
+    const result = await search();
+    for (let i of result) {
+        push(i);
+    }
 };
 
 const search = () => {
-  return new Promise(resolve => {
-    let token = [];
-    MySQL.createConnection(MySQLConfig)
-      .then((conn) => {
-        connection = conn;
-        return connection.query('select * from token');
-      }).then((result) => {
-      for (let i of result) {
-        token.push(i.token);
-      }
-      connection.end();
-      return resolve(token);
+    return new Promise(resolve => {
+        let token = [];
+        MySQL.createConnection(MySQLConfig)
+            .then((conn) => {
+                connection = conn;
+                return connection.query('select * from token');
+            }).then((result) => {
+            for (let i of result) {
+                if (i.token !== null)
+                    token.push(i.token);
+            }
+            connection.end();
+            return resolve(token);
+        });
     });
-  });
 };
 
 exports.check = async () => {
-  const checkEmergency = await emergency();
-  return parseInt(checkEmergency) > 140;
-
+    const checkEmergency = await emergency();
+    return checkStddev(checkEmergency);
 };
 
+let over = 0;
+let under = 0;
 const emergency = () => {
-  return new Promise(resolve => {
-    MySQL.createConnection(MySQLConfig)
-      .then((conn) => {
-        connection = conn;
-        return connection.query('select * from sensing where sensorId = 7 ORDER BY inputtime desc limit 1')
-      })
-      .then((result) => {
-        for (let i of result) {
-          resolve(i.sensingValue);
-        }
-      });
-  });
+    return new Promise(resolve => {
+        MySQL.createConnection(MySQLConfig)
+            .then((conn) => {
+                connection = conn;
+                return connection.query('select stddev(sensingValue) stddev from sensing where inputtime + 0 > current_timestamp() - 10 AND sensorId=3;')
+            })
+            .then((result) => {
+                for (let i of result) {
+                    if (i.stddev === null) {
+                        resolve(0);
+                    }
+                    resolve(i.stddev);
+                }
+                connection.end();
+            });
+    });
 };
+
+function checkStddev(stddev) {
+    if (stddev > 130) {
+        over++;
+    } else if (stddev < 50 && over > 4) {
+        under++;
+    }
+    if (under > 4 && over > 0 && over < 10) {
+        over = 0;
+        under = 0;
+        return pulse();
+    } else if (under > over) {
+        over = 0;
+        under = 0;
+    }
+    return false;
+}
+
+function pulse() {
+    return new Promise(resolve => {
+        MySQL.createConnection(MySQLConfig)
+            .then((conn) => {
+                connection = conn;
+                return connection.query('select * from sensing where sensorId = 7 ORDER BY inputtime desc limit 1')
+            })
+            .then((result) => {
+                console.log(result);
+                for (let i of result) {
+                    if (i.sensingValue === null) {
+                        resolve(false);
+                    } else if (i.sensingValue > 200 || i.sensingValue < 100) {
+                        resolve(true);
+                    }
+                }
+                connection.end();
+                resolve(false);
+            });
+    });
+}
 
 const push = (token) => {
-  request.post({
-    headers: {
-      'Authorization': 'key=insert server key',
-      'Content-Type': 'application/json'
-    },
-    url: 'https://fcm.googleapis.com/fcm/send',
-    body: '{"to":"' + token + '", "priority":"high","data":{"status": "최선호님의 상태가 매우 위급합니다. 119에 신고해주세요!!!!!"}}'
-  }, function (error, response, body) {
-    console.log(body);
-  });
+    request.post({
+        headers: {
+            'Authorization': 'key=(your key)',
+            'Content-Type': 'application/json'
+        },
+        url: 'https://fcm.googleapis.com/fcm/send',
+        body: '{"to":"' + token + '", "priority":"high","data":{"status": "최선호님의 상태가 매우 위급합니다. 119에 신고해주세요!!!!!"}}'
+    }, function (error, response, body) {
+        console.log(body);
+    });
 };
